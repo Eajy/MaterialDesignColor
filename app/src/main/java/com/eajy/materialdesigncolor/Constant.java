91f8737561fe169a3173023b027d5772a033b57e
Eajy <zhangxiao3919@gmail.com>
2017-04-29 18:38:55 +0800

    V1.1

--- app/src/main/java/com/eajy/materialdesigncolor/Constant.java
+++ app/src/main/java/com/eajy/materialdesigncolor/Constant.java
@@ -8,7 +8,7 @@ public class Constant {
     public static String APP_URL = "https://play.google.com/store/apps/details?id=com.eajy.materialdesigncolor";
     public static String DESIGNED_BY = "Designed by Eajy in China";
     public static String SHARE_CONTENT = "Material Design Color:\n" + APP_URL + "\n- " + DESIGNED_BY;
-    public static String EMAIL = "mailto:zhangxiao.eajy@gmail.com";
+    public static String EMAIL = "mailto:eajy.zhangxiao@gmail.com";
     public static String GIT_HUB = "https://github.com/Eajy/MaterialDesignColor";
 
     public static String MATERIAL_DESIGN_DEMO_URL = "https://play.google.com/store/apps/details?id=com.eajy.materialdesigndemo";
